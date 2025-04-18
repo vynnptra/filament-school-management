1a1b2ef9606a27a9c714600fa395fb16f0f8e87e
vynnptra <mryu.uiiiii@gmail.com>
2025-04-18 20:14:39 +0700

    8. Create Option Form

--- app/Filament/Resources/TeacherResource/RelationManagers/ClassroomRelationManager.php
+++ app/Filament/Resources/TeacherResource/RelationManagers/ClassroomRelationManager.php
@@ -5,13 +5,16 @@ namespace App\Filament\Resources\TeacherResource\RelationManagers;
 use App\Models\Classroom;
 use App\Models\Periode;
 use Filament\Forms;
+use Filament\Forms\Components\Actions\Action;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Form;
+use Filament\Forms\Set;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class ClassroomRelationManager extends RelationManager
 {
@@ -22,10 +25,37 @@ class ClassroomRelationManager extends RelationManager
         return $form
             ->schema([
                 Select::make('classrooms_id')
+                    ->relationship('classroom', 'name')
+                    ->createOptionForm([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->afterStateUpdated( fn ( Set $set, ?string $state) => $set('slug', Str::slug($state))),
+                        Forms\Components\Hidden::make('slug'),
+                    ])
+                    ->createOptionAction(
+                        function (Action $action) {
+                            return $action
+                            ->label(label: 'Create Classroom')
+                            ->modalHeading('Create Classroom');
+                        }
+                    )
                     ->label('Classroom')
                     ->options(Classroom::all()->pluck('name', 'id'))
                     ->searchable(),
                 Select::make('periode_id')
+                    ->relationship('periode', 'name')
+                    ->createOptionForm([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->afterStateUpdated( fn ( Set $set, ?string $state) => $set('slug', Str::slug($state))),
+                    ])
+                    ->createOptionAction(
+                        function (Action $action) {
+                            return $action
+                            ->label(label: 'Create Periode')
+                            ->modalHeading('Create Periode');
+                        }
+                    )
                     ->label('Periode')
                     ->options(Periode::all()->pluck('name', 'id'))
                     ->searchable(),
